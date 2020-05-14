a84e4630512aa010b8a0ea812b1ed37400607d5b
Obonic <obonic@gmail.com>
2020-05-14 15:03:21 -0700

    More Files

---
+++ QuotesApi/QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Models;
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        List<Quote> _quotes = new List<Quote>()
+        {
+           new Quote(){ID=0,Author="Me",Description="Yep!", Title="THe test Game0"},
+           new Quote(){ID=1,Author="YOU",Description="Yep!", Title="THe test Game1"},
+           new Quote(){ID=3,Author="THEM",Description="Yep!", Title="THe test Game2"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+    }
+}
---
+++ QuotesApi/QuotesApi/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesApi.Models
+{
+    public class Quote
+    {
+        public int ID { get; set; }
+
+        public int Title { get; set; }
+
+        public int Author { get; set; }
+
+        public int Description { get; set; }
+    }
+}
